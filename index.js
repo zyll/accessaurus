c33ec44310ab51a476c51392d62f0a50c690e372
zyll <gilles.robit@af83.com>
2014-01-07 22:20:54 +0100

    growing to death

--- index.js
+++ index.js
@@ -6,17 +6,36 @@ var Dinosaurus = function() {
   events.EventEmitter.call(this);
   Object.defineProperty(this, 'height', {
     get: function() {
-      console.log('getting height : ', this._height);
       return this._height;
     },
     set: function(value) {
       if (value !== this._height) {
         this._height = value;
+        if(value > 5) {
+          return this.alive = false;
+        }
         this.emit('change:height', this);
       }
     }
   });
   this._height = 0;
+  Object.defineProperty(this, 'alive', {
+    get: function() {
+      return this._alive;
+    },
+    set: function(value) {
+      if (this._alive && !value) {
+        this._alive = false;
+        this.emit('dead');
+      }
+    }
+  });
+  Object.defineProperty(this, 'dead', {
+    get: function() {
+      return !this._alive;
+    },
+  });
+  this._alive = true;
 };
 
 util.inherits(Dinosaurus, events.EventEmitter);
--- test.js
+++ test.js
@@ -4,6 +4,11 @@ var dédé = new Dinosaurus();
 dédé.on('change:height', function(){
   console.log('dédé height change to ' + dédé.height);
 });
-dédé.height;
-dédé.height = 42;
-dédé.height;
+
+var id = setInterval(function(){
+  dédé.height++;
+}, 1000);
+dédé.on('dead', function() {
+  if(dédé.dead) console.log('poor dédé');
+  clearInterval(id);
+});
