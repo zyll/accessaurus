3df8f5665f539e8a93bbdea2e4223e79ffac5566
zyll <gilles.robit@af83.com>
2014-01-07 21:22:38 +0100

    create dédé, setting height then getting height

---
+++ index.js
@@ -0,0 +1,13 @@
+// defining a growing af83 dinosaurus rus
+
+var Dinosaurus = function() {
+  Object.defineProperty(this, 'height', {
+    get: function() {
+      console.log('getting height : ', this._height);
+      return this._height;
+    },
+    set: function(value) {this._height = value;}
+  });
+};
+
+module.exports = Dinosaurus;
---
+++ test.js
@@ -0,0 +1,6 @@
+var Dinosaurus = require('./index');
+console.log(Dinosaurus);
+
+var dédé = new Dinosaurus();
+dédé.height = 42;
+dédé.height;
